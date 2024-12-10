880759baf0fb40395fe671c6a9bf65dd8e15c2fc
Thomas Saint-Gérand <devspaceship@gmail.com>
2024-12-10 21:56:42 +0100

    test managing dotfile with home-manager

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -3,12 +3,11 @@
 #   imports = lib.optionals pkgs.stdenv.isLinux [ ./linux.nix ]
 #     ++ lib.optionals pkgs.stdenv.isDarwin [ ./darwin.nix ];
 # }
-#
 
 { config, pkgs, ... }:
-
 {
   home.username = "devspaceship";
+  # home.homeDirectory = "/home/devspaceship";
   home.homeDirectory = "/Users/devspaceship";
   home.stateVersion = "24.11";
 
@@ -28,29 +27,13 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+    ".config/htop/htoprc".source = dotfiles/htop/htoprc;
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/devspaceship/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
 
