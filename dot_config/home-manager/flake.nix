d71bc9209eb554cabf3e6c8f5dface9b140af68b
Thomas Saint-Gérand <devspaceship@gmail.com>
2024-12-10 21:35:39 +0100

    [home-manager] merged files

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -2,28 +2,31 @@
   description = "Home Manager configuration of devspaceship";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, nixgl, ... }:
     let
+      # system = "x86_64-linux";
       system = "x86_64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ nixgl.overlay ];
+      };
+    in
+    {
       homeConfigurations."devspaceship" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
+
