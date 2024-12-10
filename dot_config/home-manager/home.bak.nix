8cc76e87a313744fa0970391d72264f3a0672829
Thomas Saint-Gérand <devspaceship@gmail.com>
2024-12-10 21:26:42 +0100

    experiment cross platform home manager flake

--- dot_config/home-manager/home.bak.nix
+++ dot_config/home-manager/home.bak.nix
@@ -1,70 +1,62 @@
-{ config, lib, pkgs, ... }:
-
+{ lib, pkgs, ... }:
 {
-  home.username = "devspaceship";
-  home.homeDirectory = "/home/devspaceship";
+  imports = lib.optionals pkgs.stdenv.isLinux [ ./linux.nix ]
+    ++ lib.optionals pkgs.stdenv.isDarwin [ ./darwin.nix ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  # home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.username = "devspaceship";
   home.stateVersion = "25.05";
 
-  nixGL.packages = import <nixgl> { inherit pkgs; };
-  nixGL.defaultWrapper = "mesa";
-  nixGL.offloadWrapper = "nvidiaPrime";
-  nixGL.installScripts = [ "mesa" "nvidiaPrime" ];
-
   home.packages = with pkgs; [
-    (config.lib.nixGL.wrap kitty)
-    htop
-    ripgrep
-    fzf
-    lazygit
     cmatrix
+    fzf
+    htop
+    lazygit
+    neovim
+    ripgrep
     tree
     # (nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/devspaceship/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
+
+
+# {
+#   # Home Manager is pretty good at managing dotfiles. The primary way to manage
+#   # plain files is through 'home.file'.
+#   home.file = {
+#     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+#     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+#     # # symlink to the Nix store copy.
+#     # ".screenrc".source = dotfiles/screenrc;
+#
+#     # # You can also set the file content immediately.
+#     # ".gradle/gradle.properties".text = ''
+#     #   org.gradle.console=verbose
+#     #   org.gradle.daemon.idletimeout=3600000
+#     # '';
+#   };
+#
+#   # Home Manager can also manage your environment variables through
+#   # 'home.sessionVariables'. These will be explicitly sourced when using a
+#   # shell provided by Home Manager. If you don't want to manage your shell
+#   # through Home Manager then you have to manually source 'hm-session-vars.sh'
+#   # located at either
+#   #
+#   #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+#   #
+#   # or
+#   #
+#   #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+#   #
+#   # or
+#   #
+#   #  /etc/profiles/per-user/devspaceship/etc/profile.d/hm-session-vars.sh
+#   #
+#   home.sessionVariables = {
+#     # EDITOR = "emacs";
+#   };
+#
+#   # Let Home Manager install and manage itself.
+#   programs.home-manager.enable = true;
+# }
